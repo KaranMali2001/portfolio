247590f9320885fd7622da9a6295a9b464489c6b
karan <karanmali122001@gmail.com>
2025-11-01 22:07:08 +0530

    Updated site icon

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,4 @@
-import MetadataRoute from "next";
-
-export default function sitemap(): MetadataRoute.Sitemap {
+export default function sitemap() {
   const baseUrl = "https://karan5599.tech";
 
   // Get current date for lastModified
